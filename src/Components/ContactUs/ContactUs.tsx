70669020babdcbb3d3e43cfb8e8e2735bca1dc33
Tushar Gautam <tushargautam999@gmail.com>
2025-03-01 22:44:23 +0530

    embeded location

--- src/Components/ContactUs/ContactUs.tsx
+++ src/Components/ContactUs/ContactUs.tsx
@@ -104,14 +104,14 @@ export default function ContactSection() {
         <div className="map-container">
           <div className="map-container__glass" />
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387193.30596073366!2d-74.25986548248684!3d40.69714941932609!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2s!4v1647043435559!5m2!1sen!2s"
-            width="100%"
-            height="100%"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14232.989586360334!2d75.7256215395508!3d26.895642500000015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396db5f1b4c6f263%3A0xb8174c5e55aea7fa!2sKaushik%20House%20-%20Luxury%20Homestay!5e0!3m2!1sen!2sin!4v1740849162541!5m2!1sen!2sin"
+            width="600"
+            height="450"
             style={{ border: 0 }}
             allowFullScreen
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
-          />
+          ></iframe>
         </div>
       </div>
     </section>

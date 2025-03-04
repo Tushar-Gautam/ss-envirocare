cb7f81ac03a035dd4ea8ef7aa05a1fe666229845
Tushar Gautam <tushargautam999@gmail.com>
2025-03-05 00:45:18 +0530

    e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
-import Navbar from "@/Components/Navbar";
+// import Navbar from "@/Components/Navbar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
